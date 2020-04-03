d3460c5ad2dd0bb9115b3d400d581c30d16b0d7a
mldn <ljl@qq.com>
2020-04-03 11:41:11 +0800

    udpate b.java and del c.java

--- c.java
+++
@@ -1,9 +0,0 @@
-public class c{
-	public static void public static void main(String[] args) {
-		System.out.println("c");
-System.out.println("c");
-System.out.println("c");
-System.out.println("c");
-
-	}
-}master d
