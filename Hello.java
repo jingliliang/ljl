314e94e58f1754a9d6efd1855b294075930c542c
mldn <ljl@qq.com>
2020-04-03 10:20:49 +0800

    update Hello.java in master

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello{
 	public static void public static void main(String[] args) {
+		System.out.println("dev");
 		System.out.println("Hello World");
 		System.out.println("new add Hello World");
+		System.out.println("add new");
+		System.out.println("dev");
 	}
 }sfs
