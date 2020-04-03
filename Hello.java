58957c1940af1b20f23acd16779d9162c1abe41e
mldn <ljl@qq.com>
2020-04-03 10:12:33 +0800

    add dev.txt and update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("new add Hello World");
+		System.out.println("dev");
 	}
 }sfs
