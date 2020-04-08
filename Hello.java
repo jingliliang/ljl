65b1397547f4889b04a708b8781aed453e990b8e
mldn <ljl@qq.com>
2020-04-08 12:27:35 +0800

    d ds

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello{
 	public static void public static void main(String[] args) {
 		System.out.println("dev");
-
-		System.out.println("Hello World");
-		System.out.println("new add Hello World");
-	
-System.out.println("dev");
-System.out.println("errot");
+System.out.println("dev");System.out.println("dev");System.out.println("dev");
 
 	}
 }sfs
